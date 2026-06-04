4ae4b26f1501eb0bfdc6ff531ad21d6543f72590
AmosHuKe <amoshuke@qq.com>
2026-06-04 03:50:56 +0000

    Deploying to github-pages from @ AmosHuKe/flutter_mosaic@1d5046698ef3864f04d7705f0e571b5882de7e24 🚀

--- main.dart.js
+++ main.dart.js
@@ -22519,7 +22519,7 @@ $iQD:1}
 A.ac7.prototype={
 $1(a){var s=A.co().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/4c525dac5ebe5971c5708ef73558ed8edcf4a362/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/c416acfeb8126e097f758c664aaa3da929e27da0/":s)+a},
 $S:95}
 A.El.prototype={
 gv(a){var s=this.a
