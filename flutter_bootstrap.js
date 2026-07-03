4c62726ca319f227426f1d37c5dbb8a5b16a583a
AmosHuKe <amoshuke@qq.com>
2026-07-03 04:53:48 +0000

    Deploying to github-pages from @ AmosHuKe/flutter_mosaic@9e9b940720dac51041ec393b2f82de4a343b99a1 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,7 +43,7 @@ _flutter.loader.load({
     canvasKitBaseUrl: '/flutter_mosaic/canvaskit/'
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1176012275" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "2688192830" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
   onEntrypointLoaded: async function (engineInitializer) {
     const appRunner = await engineInitializer.initializeEngine();
